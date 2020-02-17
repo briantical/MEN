187116dbc3082d31256eb2920d0e7696264a85ca
briantical <lutbrianivan@gmail.com>
2020-02-17 09:36:39 +0300

    Fixing deprecation errors

--- src/mongo/MongoManager.js
+++ src/mongo/MongoManager.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
 
 class MongoManager {
   constructor() {}
