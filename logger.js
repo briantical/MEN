22a8da054fd40b9c57880135856772438e7518d4
briantical <lutbrianivan@gmail.com>
2020-02-17 08:47:28 +0300

    Fix 'TypeError: winston.Logger is not a constructor'

--- logger.js
+++ logger.js
@@ -1,11 +1,11 @@
-const winston = require('winston');
-require('winston-mongodb');
-const { mongoManager } = require('./src/mongo/MongoManager');
+const winston = require("winston");
+require("winston-mongodb");
+const { mongoManager } = require("./src/mongo/MongoManager");
 
-const logger = new (winston.Logger)({
+const logger = winston.createLogger({
   transports: [
-    new (winston.transports.MongoDB)({ db: mongoManager.getMongoUrl() }),
-  ],
+    new winston.transports.MongoDB({ db: mongoManager.getMongoUrl() })
+  ]
 });
 
 module.exports = logger;
