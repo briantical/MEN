7b2202fa2161671ae2abb0d5e80939b2277d7af9
briantical <lutbrianivan@gmail.com>
2020-02-18 16:25:58 +0300

    assets/model.json: Unexpected token } in JSON at position 24

--- app.js
+++ app.js
@@ -1,61 +1,57 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const fs = require('fs');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const fs = require("fs");
+const cookieParser = require("cookie-parser");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const SwaggerExpress = require('swagger-express-mw');
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerMerger = require('swagger-merger')
+const SwaggerExpress = require("swagger-express-mw");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerMerger = require("swagger-merger");
 
-const bodyParser = require('body-parser');
-const { config } = require('./config');
-const api = require('./src/api/index');
-const { passport } = require('./src/passport');
-const { mongoManager } = require('./src/mongo');
-const { onAppStart } = require('./on-start');
-
-
+const bodyParser = require("body-parser");
+const { config } = require("./config");
+const api = require("./src/api/index");
+const { passport } = require("./src/passport");
+const { mongoManager } = require("./src/mongo");
+const { onAppStart } = require("./on-start");
 
 const app = express();
 mongoManager.connect();
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 // middleware
-app.use(bodyParser.json({
-  limit: config.bodyLimit,
-}));
+app.use(
+  bodyParser.json({
+    limit: config.bodyLimit
+  })
+);
 
 // Authorization
 app.use(passport.init());
 
 // api routes v1
-app.use('/api/v1', api(config));
-
+app.use("/api/v1", api(config));
 
 // register api doc
-const outputSwaggerDir = path.resolve(config.swaggerDirPath, './build');
-const swaggerBuildFilePath = path.resolve(outputSwaggerDir, './swagger.yaml');
+const outputSwaggerDir = path.resolve(config.swaggerDirPath, "./build");
+const swaggerBuildFilePath = path.resolve(outputSwaggerDir, "./swagger.yaml");
 
-if(!fs.existsSync(outputSwaggerDir)) {
-	fs.mkdirSync(outputSwaggerDir);
+if (!fs.existsSync(outputSwaggerDir)) {
+  fs.mkdirSync(outputSwaggerDir);
 }
 swaggerMerger({ input: config.swaggerFilePath, output: swaggerBuildFilePath });
 const swaggerDocument = YAML.load(swaggerBuildFilePath);
-app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // on App start
-//onAppStart();
-
+onAppStart();
 
 module.exports = app;
